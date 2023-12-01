b07a3f43fb18f91925bf8a827f3d21503cae29f2
Rdm <108627995+rinomakin@users.noreply.github.com>
2023-12-01 10:35:26 +0600

    Update private.client.js

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://movie-2024-api.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
